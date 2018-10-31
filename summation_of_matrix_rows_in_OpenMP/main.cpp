b045afc248df05a2b052b0af3ca686ca9a3db6ec
savoptik <sav70792@gmail.com>
2018-10-31 10:25:08 +0300

    Initial Commit

---
+++ summation_of_matrix_rows_in_OpenMP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  summation_of_matrix_rows_in_OpenMP
+//
+//  Created by Артём Семёнов on 31/10/2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
